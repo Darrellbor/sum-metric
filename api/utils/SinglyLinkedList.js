f18605d97a6ac873ab85f94f5d192004d5a71e9c
Darrel Idiagbor <idiagbordarrel@gmail.com>
2020-05-05 16:17:02 +0100

    initial commit and folder structure

---
+++ api/utils/SinglyLinkedList.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+export class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    add(val) {
+        
+    }
+}
